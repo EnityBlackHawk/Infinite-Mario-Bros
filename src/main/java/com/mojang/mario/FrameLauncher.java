b5172fe0c24b68ea352817597de9b7c6b3ecb5bc
Luan Felipe Marmentini <luanf2003@gmail.com>
2025-05-02 12:35:14 -0300

    build: Update Java version

--- src/main/java/com/mojang/mario/FrameLauncher.java
+++ src/main/java/com/mojang/mario/FrameLauncher.java
@@ -8,12 +8,18 @@ public class FrameLauncher
     public static void main(String[] args)
     {
         MarioComponent mario = new MarioComponent(640, 480);
+        GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice device = graphics.getDefaultScreenDevice();
+
         JFrame frame = new JFrame("Infinite Mario Bros.");
+        frame.setUndecorated(true);
         frame.setContentPane(mario);
         frame.pack();
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
+        device.setFullScreenWindow(frame);
+
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation((screenSize.width-frame.getWidth())/2, (screenSize.height-frame.getHeight())/2);
         
